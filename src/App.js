29bd47c8ad43162f1418f06d285d09bfa5138b38
Bradley Neve <brad.neve@gmail,com>
2022-09-29 13:33:20 +0100

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+import { Header, Footer } from './layout/index'
+
+
+const App = () => {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+        <div style={{ backgroundColor:'#505760', color: '#cbd3e0', margin: '0', padding: '10px'}}>
+          <Header />
+          {/* <Footer /> */}
+
+        </div>
+    </>
+  )
+};
 
 export default App;
---
+++ src/layout/Header/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import './style.css'
+
+export function Header() {
+    const activeClass = (({ isActive }) =>
+        isActive ? 'current' : 'navFont')
+
+
+    return (
+        <>
+            <ul>
+                <NavLink className={activeClass} to="/" end>Home</NavLink>
+                <NavLink className={activeClass} to="about" end>About Me</NavLink>
+                <NavLink className={activeClass} to="projects" end>Projects</NavLink>
+            </ul>
+        </>
+
+
+    );
+}
