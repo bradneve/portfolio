791282d5d2d8d4f084033bf09e112c47039c05ad
Bradley Neve <brad.neve@gmail,com>
2022-10-06 17:12:01 +0100

    add habitual card

--- src/components/Album/index.js
+++ src/components/Album/index.js
@@ -2,7 +2,8 @@
 import * as React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import CssBaseline from '@mui/material/CssBaseline';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
@@ -22,7 +23,7 @@ export default function Album() {
         <ThemeProvider theme={theme}>
             <CssBaseline />
 
-            <main>
+            <main >
                 {/* Hero unit */}
                 <Box
                     sx={{
@@ -54,48 +55,21 @@ export default function Album() {
                         </Stack>
                     </Container>
                 </Box>
-                <CardGroup>
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This card has supporting text below as a natural lead-in to
-            additional content.{' '}
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src="holpic.PNG" />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This card has even longer content than the
-            first to show that equal height action.
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-    </CardGroup>
+                <Row style={{padding: '50px'}} xs={1} md={2} className="g-4">
+                    
+                        <Col>
+                            <Card style={{backgroundColor: '#cbd3e0', color: '#505760'}}>
+                                <Card.Img variant="top" src="habitual.png" />
+                                <Card.Body>
+                                    <Card.Title>Habitual</Card.Title>
+                                    <Card.Text>
+                                        Habitual was built as part of a group project. It allows you to, upon login, set (and subsequently track) habits you wish to track.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                </Row>
+                
             </main>
 
         </ThemeProvider>
